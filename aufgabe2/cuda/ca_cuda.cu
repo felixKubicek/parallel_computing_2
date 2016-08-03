b41e4406a457f3a01b3ef3cf96f2fd82899feb15
Felix Kubicek <afc89@gpu2.haiti.cs.uni-potsdam.de>
2016-08-03 16:10:51 +0200

    simulate now executed as kernel

--- aufgabe2/cuda/ca_cuda.cu
+++ aufgabe2/cuda/ca_cuda.cu
@@ -12,41 +12,38 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "ca_common.h"
+
+#define BLOCK_SIZE_X 32
+#define BLOCK_SIZE_Y 20
+
+#define CUDA_ERROR_CHECK(x)\
+    do {cudaError_t last_err = (x);\
+        if (last_err != cudaSuccess)\
+                {fprintf(stderr ,"%s:%u: CUDA error: %s\n", __FILE__, __LINE__, cudaGetErrorString ( last_err )); exit(EXIT_FAILURE); }\
+    } while (false)
+
+#define MALLOC_ERROR_CHECK(x)\
+        do {\
+            if ( (x) == NULL)\
+                {fprintf(stderr ,"%s:%u: malloc error!\n", __FILE__, __LINE__); exit (EXIT_FAILURE); }\
+        } while (false)
+
 /* --------------------- CA simulation -------------------------------- */
 
 /* annealing rule from ChoDro96 page 34
  * the table is used to map the number of nonzero
  * states in the neighborhood to the new state
  */
-static const cell_state_t anneal[10] = {0, 0, 0, 0, 1, 0, 1, 1, 1, 1};
-
-/* treat torus like boundary conditions */
-static void boundary(line_t *buf, int lines)
-{
-	for (int y = 0; y <= lines + 1; y++) {
-		/* copy rightmost column to the buffer column 0 */
-		buf[y][0] = buf[y][XSIZE];
-
-		/* copy leftmost column to the buffer column XSIZE + 1 */
-		buf[y][XSIZE + 1] = buf[y][1];
-	}
-
-	for (int x = 0; x <= XSIZE + 1; x++) {
-		/* copy bottommost row to buffer row 0 */
-		buf[0][x] = buf[lines][x];
-
-		/* copy topmost row to buffer row lines + 1 */
-		buf[lines + 1][x] = buf[1][x];
-	}
-}
+__constant__ static const cell_state_t anneal[10] = {0, 0, 0, 0, 1, 0, 1, 1, 1, 1};
 
 /* make one simulation iteration with lines lines.
  * old configuration is in from, new one is written to to.
  */
-void simulate(line_t_cuda *from, line_t_cuda *to, int lines)
+__global__ static void simulate(line_t_cuda *from, line_t_cuda *to, int lines)
 {
+        // TODO: for loops for x and y dimension 
+        /*
         for (int y = 0; y < lines; y++) {
 		for (int x = 0; x < XSIZE; x++) {
 
@@ -58,20 +55,17 @@ void simulate(line_t_cuda *from, line_t_cuda *to, int lines)
 			to[y][x] = transition_cuda(from, x_prev, y_prev, x, y, x_next, y_next);
 		}
 	}
+        */
 
-        /*
-        int x = (threadIdx.x + blockIdx.x * blockDim.x) + 1;
-        int y = (threadIdx.y + blockIdx.y * blockDim.y) + 1;
+        int x = threadIdx.x + blockIdx.x * blockDim.x;
+        int y = threadIdx.y + blockIdx.y * blockDim.y;
         
         int x_prev = ((x - 1) + XSIZE) % XSIZE;
         int y_prev = ((y - 1) + lines) % lines;
         int x_next = (x + 1) % XSIZE; 
         int y_next = (y + 1) % lines; 
 
-        to[y][x] = transition_torus(from, x_prev, y_prev, x, y, x_next, y_next);
-        */
-
-        // TODO: for loops for x and y dimension 
+        to[y][x] = transition_cuda(from, x_prev, y_prev, x, y, x_next, y_next);
 }
 
 /* --------------------- measurement ---------------------------------- */
@@ -83,33 +77,49 @@ int main(int argc, char** argv)
 	ca_init(argc, argv, &lines, &its);
 
         line_t_cuda *from, *to, *from_d, *to_d;
+        line_t *verify_field;
 
-	from = (line_t_cuda *) calloc(lines, sizeof(line_t_cuda));
-	to = (line_t_cuda *) calloc(lines, sizeof(line_t_cuda));
+	MALLOC_ERROR_CHECK(from = (line_t_cuda *) calloc(lines, sizeof(line_t_cuda)));
+	MALLOC_ERROR_CHECK(to = (line_t_cuda *) calloc(lines, sizeof(line_t_cuda)));
+	MALLOC_ERROR_CHECK(verify_field = (line_t *) calloc((lines + 2), sizeof(line_t)));
+
+        CUDA_ERROR_CHECK(cudaMalloc((void **) &from_d, lines * sizeof(line_t_cuda)));
+        CUDA_ERROR_CHECK(cudaMalloc((void **) &to_d, lines * sizeof(line_t_cuda)));
+        CUDA_ERROR_CHECK(cudaMalloc((void **) &to_d, lines * sizeof(line_t_cuda)));
 
 	ca_init_config_cuda(from, lines, 0);
+        CUDA_ERROR_CHECK(cudaMemcpy((void *) from_d, (void *) from, lines * sizeof(line_t_cuda), cudaMemcpyHostToDevice));
+        CUDA_ERROR_CHECK(cudaMemcpy((void *) to_d, (void *) to, lines * sizeof(line_t_cuda), cudaMemcpyHostToDevice));
+
+        dim3 dimBlock(BLOCK_SIZE_X, BLOCK_SIZE_Y);
+        dim3 dimGrid(XSIZE/dimBlock.x, lines/dimBlock.y);
 
 	TIME_GET(sim_start);
 	for (int i = 0; i < its; i++) {
-		simulate(from, to, lines);
+		simulate <<<dimGrid, dimBlock>>> (from_d, to_d, lines);
 
-		line_t_cuda *temp = from;
-		from = to;
-		to = temp;
+		line_t_cuda *temp = from_d;
+		from_d = to_d;
+		to_d = temp;
 	}
 	TIME_GET(sim_stop);
 
-	line_t *verify_field = (line_t *) calloc((lines + 2), sizeof(line_t));
+        CUDA_ERROR_CHECK(cudaPeekAtLastError());
+        CUDA_ERROR_CHECK(cudaMemcpy((void *) from, (void *) from_d, lines * sizeof(line_t_cuda), cudaMemcpyDeviceToHost));
 
         for(int y = 1; y <= lines; y++)
         {
             memcpy((void *) &verify_field[y][1], (void *) &from[y-1][0], XSIZE);
         }
-        
+
+        //print_field(verify_field, lines);
 	ca_hash_and_report(verify_field + 1, lines, TIME_DIFF(sim_start, sim_stop));
 
 	free(from);
 	free(to);
+        free(verify_field);
+        CUDA_ERROR_CHECK(cudaFree(from_d));
+        CUDA_ERROR_CHECK(cudaFree(to_d));
 
 	return EXIT_SUCCESS;
 }
